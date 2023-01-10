fbd3c97ff54b2d49647bbfbd0ce70c3e1f10b6f5
Phenc <97491430+cuicuijiamin@users.noreply.github.com>
2023-01-10 20:15:47 +0800

    Create P5020 货币系统.cpp

---
+++ 动态规划/背包/P5020 货币系统.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int f[25005];
+int a[105];
+int main()
+{
+    //freopen("money.in","r",stdin);
+    //freopen("money.out","w",stdout);
+    int i,j,n,T,ans;
+    scanf("%d",&T);
+    while(T--)
+    {
+        memset(f,0,sizeof(f));
+        scanf("%d",&n);
+		ans=n;
+        for(i=1;i<=n;i++) scanf("%d",&a[i]);
+        sort(a+1,a+n+1);
+        f[0]=1;
+        for(i=1;i<=n;i++)
+        {
+            if(f[a[i]])
+            {
+                ans--;
+                continue;
+            }
+            for(j=a[i];j<=a[n];j++)
+            {
+                f[j]+=f[j-a[i]];
+            }
+        }
+        printf("%d\n",ans);
+    }
+    return 0;
+}
